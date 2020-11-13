763cbf616dfe99e20295d23c6682dfc03bfff8dc
wenyajun2011 <153165057@qq.com>
2020-11-13 22:49:40 +0800

    444444

--- src/components/demo.vue
+++ src/components/demo.vue
@@ -1,6 +1,6 @@
 <template>
  <div>
-     <p>222ddfddfdd</p>
+     
  </div>
 </template>
 
@@ -8,7 +8,7 @@
  export default {
    data () {
      return {
-
+         aa:'123'
      }
    },
    components: {
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-   chainWebpack:config => {
-       config.resolve.symlinks(true)
-   },
-   css:{
-       extract:true
-   }
+    devServer:{
+        host: "0.0.0.0",
+        port: 8080, // 端口号
+        https: false, // https:{type:Boolean}
+        open: false, //配置自动启动浏览器  
+        hot: true, // 热更新
+    },
+    runtimeCompiler:true,
+    chainWebpack: config => {        // 修复HMR
+        config.resolve.symlinks(true)
+    },
+    css: {  
+        extract: true,
+    }
 }
